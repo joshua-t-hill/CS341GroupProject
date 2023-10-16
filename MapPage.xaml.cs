b6401e8b3f87a34b9bdd007f36727ff0302edf4e
joshua-t-hill <145697719+joshua-t-hill@users.noreply.github.com>
2023-10-16 18:58:50 -0500

    Map location opens on UW Oshkosh campus

--- MapPage.xaml.cs
+++ MapPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Maui.Maps;
 using Map = Microsoft.Maui.Controls.Maps.Map;
 /**
  * Author: Joshua T. Hill
@@ -9,7 +10,10 @@ public partial class MapPage : ContentPage
 
     public MapPage()
     {
-        Map map = new();
+        Location location = new Location(44.0263, -88.5507);
+        MapSpan mapSpan = new MapSpan(location, 0.01, 0.01);
+        Map map = new(mapSpan);
+
         Content = map;
     }
 
