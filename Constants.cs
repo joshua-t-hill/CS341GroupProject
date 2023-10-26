4b08658acc807e5ce333fab869a85aaf75721e0c
joshua-t-hill <hill.joshuataylor@gmail.com>
2023-10-26 14:45:47 -0500

    Added Constants.cs file; populated login info for project DB

---
+++ Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace CS341Lab3;
+/// <summary>
+/// Strings and warnings that come up repeatedly.
+/// </summary>
+public static class Constants
+{
+    //Database connection information
+    public const string DbHost = "cs341-labserver-13086.5xj.cockroachlabs.cloud";
+    public const int DbPort = 26257;
+    public const string DbName = "project_group_3_db";
+    public const string DbApp = "group_project"; //optional
+    public const string DbUser = "hill_joshuataylor_gm";
+    public const string DbPass = "iZyAvF0iGFv553Ahf-yvGg";
+
+    //SQL Queries and Commands
+
+}
