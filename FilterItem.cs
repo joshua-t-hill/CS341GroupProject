6357da3d46ffb33e3c6cf830dd15411f931882d5
joshua-t-hill <145697719+joshua-t-hill@users.noreply.github.com>
2023-10-28 18:05:09 -0500

    Did some reorganizing; moved all pages besides App and AppShell into "Pages" folder; moved FilterItem.cs into "Model" folder

--- FilterItem.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CS341GroupProject;
-public class FilterItem
-{
-    public String Photo { get; set; }
-    public String Plant { get; set; }
-}
---
+++ Model/FilterItem.cs
@@ -0,0 +1,6 @@
+﻿namespace CS341GroupProject.Model;
+public class FilterItem
+{
+    public string Photo { get; set; }
+    public string Plant { get; set; }
+}
