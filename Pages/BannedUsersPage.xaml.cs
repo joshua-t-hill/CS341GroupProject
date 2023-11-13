1244e96a5c694ad12bba3c3ae52642cc47d23a9b
Alex Ceithamer <alexceithamer@gmail.com>
2023-11-13 11:47:58 -0600

    Updating comments and UserSearchPage to reflect bans to the database
    ~ modified README.txt to use symbols indicating whether something was modified (~) or added (+) or removed (-). If something was modified to add functionality, that would be a + symbol
    ~ updated comments on BanedUsersPage.xaml.cs
    ~updated comments on UserSearchPage.xaml.cs
    +Updated methods on UserSearchPage.xaml.cs to update the database when a user is banned

--- Pages/BannedUsersPage.xaml.cs
+++ Pages/BannedUsersPage.xaml.cs
@@ -10,13 +10,16 @@ namespace CS341GroupProject
     {
         public ObservableCollection<User> bannedUsers;
 
+        /// <summary>
+        /// Default constructor, populates the banned users list and binds it to the BannedUsersListView item source.
+        /// </summary>
         public BannedUsersPage()
         {
             InitializeComponent();
 
-            //populate the banned users list
+            //Initialize ObservableCollection of banned users
             bannedUsers = new ObservableCollection<User>();
-
+            //Populate banned users list
             ObservableCollection<User> users = MauiProgram.BusinessLogic.Users;
             foreach (User user in users)
             {
@@ -25,24 +28,27 @@ namespace CS341GroupProject
                     bannedUsers.Add(user);
                 }
             }
-            
+            //Bind banned users list to BannedUsersListView item source
             BannedUsersListView.ItemsSource = bannedUsers;
         }
 
+        /// <summary>
+        /// Unban functionality, removes the selected user from the banned users list and updates the backend to reflect the change.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnUnbanClicked(object sender, EventArgs e)
         {
             var selectedUser = BannedUsersListView.SelectedItem as User;
             if (selectedUser != null)
             {
-                // Implement unbanning logic here
-                // For example, removing the user from the list:
+                //remove the user from the banned users list
                 bannedUsers.Remove(selectedUser);
+
+                //update the backend to reflect the change.
                 User oldUserInfo = selectedUser;
                 User newUserInfo = new User(oldUserInfo.Username, oldUserInfo.Password, oldUserInfo.Email, false);
                 MauiProgram.BusinessLogic.UpdateUser(oldUserInfo, newUserInfo);
-
-
-                // If there is any backend or database, we'd also remove the ban there.
             }
         }
     }
--- Pages/UserSearchPage.xaml.cs
+++ Pages/UserSearchPage.xaml.cs
@@ -12,21 +12,26 @@ namespace CS341GroupProject
         private ObservableCollection<User> AllUsers;
         public ObservableCollection<User> FilteredUsers { get; private set; }
 
+        /// <summary>
+        /// Default constructor, populates the filtered users list and binds it to the UsersListView item source. Also populates the all users list.
+        /// </summary>
         public UserSearchPage()
         {
             InitializeComponent();
-            /*
-             * TODO: Implement a way to get the list of users from the database. Refer to BannedUsersPage.xaml.cs for additional information/thoughts.
-             */
+            //get ObservableCollection of all users
             AllUsers = MauiProgram.BusinessLogic.Users;
-
-
-
-
+            //populate filtered users ObservableCollection with all users
             FilteredUsers = new ObservableCollection<User>(AllUsers);
+            //bind filtered users ObservableCollection to UsersListView item source
             UsersListView.ItemsSource = FilteredUsers;
         }
 
+        /// <summary>
+        /// Updates the filtered users list to only contain users whose username contains the search term. This implements a live 
+        /// search functionality, where the list is updated as the user types.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
         {
             var searchTerm = e.NewTextValue;
@@ -34,15 +39,29 @@ namespace CS341GroupProject
             FilteredUsers.Clear();
             foreach (var user in AllUsers.Where(u => u.Username.ToLower().Contains(searchTerm.ToLower())))
             {
-                FilteredUsers.Add(user);
+                //add user to list if not banned
+                if (!user.IsBanned) {
+                    FilteredUsers.Add(user);
+                }
             }
         }
 
+        /// <summary>
+        /// When a user is selected, enable the ban button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnUserSelected(object sender, SelectedItemChangedEventArgs e)
         {
             BanButton.IsEnabled = e.SelectedItem != null;
         }
 
+        /// <summary>
+        /// Ban functionality, When the ban button is clicked, We send a confirmation pop-up. If the user confirms, we remove the user from
+        /// the all users list and the filtered users list, then reflect changes on the backend.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void OnBanButtonClicked(object sender, EventArgs e)
         {
             var selectedUser = UsersListView.SelectedItem as User;
@@ -51,7 +70,10 @@ namespace CS341GroupProject
                 bool confirm = await DisplayAlert("Confirmation", $"Are you sure you want to ban {selectedUser.Username}?", "Yes", "No");
                 if (confirm)
                 {
-                    // TODO: Add code to ban the user, such as updating your database
+                    //update IsBanned in the backend
+                    User newUserInfo = new User(selectedUser.Username, selectedUser.Password, selectedUser.Email, true);
+                    MauiProgram.BusinessLogic.UpdateUser(selectedUser, newUserInfo);
+                    //remove user from our list
                     AllUsers.Remove(selectedUser);
                     FilteredUsers.Remove(selectedUser);
                     UsersListView.SelectedItem = null;
