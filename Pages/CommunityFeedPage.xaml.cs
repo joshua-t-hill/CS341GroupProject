3d54dc4deb28447344a505bb7d68d703a2066b7f
Alex <alexceithamer@gmail.com>
2023-11-13 23:12:38 -0600

    Prepping CommunityFeedbackPage to be functional
    - removed hard-coded item sources from xaml file
    ~ Binded the ItemsSource of the CollectionView to the Posts ObservableCollection Property
    +Set up the property binding in the cs file, and hard coded some "Posts" into the constructor

--- Pages/CommunityFeedPage.xaml.cs
+++ Pages/CommunityFeedPage.xaml.cs
@@ -1,11 +1,23 @@
+using System.Collections.ObjectModel;
+
 namespace CS341GroupProject;
 /*
  * Alexsa Walczak
  */
 public partial class CommunityFeedPage : ContentPage
 {
+	//Property to hold ObservableCollection of Post objects
+	public ObservableCollection<Post> Posts { get; set; }
 	public CommunityFeedPage()
 	{
 		InitializeComponent();
+
+		Posts = new ObservableCollection<Post>();
+
+		//example adding items to the collection
+		Posts.Add(new Post { Username = "User1", Photo = "plant.jpg", Plant = "plant1", Notes = "This is a note about plant1" });
+		Posts.Add(new Post { Username = "User2", Photo = "flower.png", Plant = "plant2", Notes = "This is a note about plant2" });
+
+		this.BindingContext = this;
 	}
 }
