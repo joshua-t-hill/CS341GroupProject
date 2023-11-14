afece4d088a8a1536378f7795110f4d0606d5a1e
Alex <alexceithamer@gmail.com>
2023-11-13 23:18:49 -0600

    Feed page comments that are "food for thought" as this page is completed

--- Pages/CommunityFeedPage.xaml.cs
+++ Pages/CommunityFeedPage.xaml.cs
@@ -20,4 +20,20 @@ public partial class CommunityFeedPage : ContentPage
 
 		this.BindingContext = this;
 	}
+
+	/*
+	 * TODO: So far, my changes (alex) appear to have added the proper binding to the feed page.
+	 * We need to have a table for posts in the database, and then implement either the constructor
+	 * to populate the ObservableCollection with the posts, or have it pull a pre-loaded observable
+	 * collection. It would depend on how things are loaded.
+	 *		If loading it here, means that it loads when switching to this tab - it may be useful to
+	 *		test performance and see if it's better to load it when we click onto the feed page or
+	 *		have it loaded at the start of the app and pull it from memory.
+	 *	
+	 *	TODO: If the posts get too long, it won't be a good idea to load all of the posts, so we may
+	 *	need to implement a way to load a certain number of posts at a time, and then load more as
+	 *	the user wishes.
+	 *		Could be done with pages, or somehow code the program to load more posts as the user 
+	 *		scrolls down. (but then if they scroll too far, would that affect performance?)
+	 */
 }
